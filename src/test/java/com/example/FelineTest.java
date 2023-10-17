39cd03117e03705c54e137c1e24dffbcd0280b2d
Alelsey Raykov <araikov@mts.ru>
2023-10-17 21:46:29 +0300

    Добавил в eatMeatTest, getFamilyTest, getKittensTestWithoutNumber, getKittensTestWith3 создание экземпляра класса Feline.

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -17,6 +18,7 @@ public class FelineTest {
 
     @Test
     public void eatMeatTest() throws Exception {
+        Feline feline = new Feline(); // исправление: добавил создание экземпляра класса в тест
         List<String> expected = List.of("Животные", "Птицы", "Рыба");
         List<String> actual = feline.eatMeat();
         Assert.assertEquals(expected, actual);
@@ -24,6 +26,7 @@ public class FelineTest {
 
     @Test
     public void getFamilyTest() {
+        Feline feline = new Feline(); // исправление: добавил создание экземпляра класса в тест
         String expected = "Кошачьи";
         String actual = feline.getFamily();
         Assert.assertEquals(expected, actual);
@@ -31,6 +34,7 @@ public class FelineTest {
 
     @Test
     public void getKittensTestWithoutNumber() {
+        Feline feline = new Feline(); // исправление: добавил создание экземпляра класса в тест
         int expected = 1;
         int actual = feline.getKittens();
         Assert.assertEquals(expected, actual);
@@ -38,6 +42,7 @@ public class FelineTest {
 
     @Test
     public void getKittensTestWith3() {
+        Feline feline = new Feline(); // исправление: добавил создание экземпляра класса в тест
         int expected = 3;
         int actual = feline.getKittens(3);
         Assert.assertEquals(expected, actual);
